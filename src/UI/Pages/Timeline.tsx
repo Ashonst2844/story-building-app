4c6b12c1a468854422adc9c91d3df8a29cd2f410
Masagus Ahmad Ramadhan <agusyantosugiyanto@gmail.com>
2026-07-21 20:02:45 +0700

    fix little bug

--- src/UI/Pages/Timeline.tsx
+++ src/UI/Pages/Timeline.tsx
@@ -9,7 +9,7 @@ interface TimelineProps {
     TimeId:string;
     title:string;
     description:string;
-    time_range:string;
+    era:string;
     related_novel:string[]
 }
 
@@ -27,7 +27,7 @@ function Timeline() {
                 {(timelines ?? []).map((time)=>(
                     <Button link={`/timeline/`+time.TimeId.toString()} key={time.TimeId} type="link" w="100%" h="150px" forNav>
                         <h3 style={{fontSize:"0.9rem", textAlign:"center"}}>{time.title}</h3>
-                        <p className="desktop-mode">{time.time_range}</p>
+                        <p className="desktop-mode">{time.era}</p>
                     </Button>
                 ))}
             </div>
@@ -38,7 +38,7 @@ function Timeline() {
                             <div className="times-content">
                                 <h1>{time.title}</h1>
                                 <hr style={{border:"1px solid var(--accent)"}}/>
-                                <h3 style={{margin:"var(--spacing) 0"}}>{time.time_range}</h3>
+                                <h3 style={{margin:"var(--spacing) 0"}}>{time.era}</h3>
                                 <p style={{textAlign:"justify"}}>{time.description}</p>
                                 <h3 style={{margin:"var(--spacing) 0"}}>Novel yang terkait:</h3>
                                 <ul>

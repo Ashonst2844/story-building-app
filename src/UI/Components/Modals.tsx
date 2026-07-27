02819be07b8c19853b1c720b362f3c7ea42fb2cb
Masagus Ahmad Ramadhan <agusyantosugiyanto@gmail.com>
2026-07-27 22:25:44 +0700

    Fix Deploy Error, Again, Again, and Again

--- src/UI/Components/Modals.tsx
+++ src/UI/Components/Modals.tsx
@@ -86,9 +86,9 @@ function Modals(props: ModalsProps) {
             </div>}
             <Button onClick={props.onClose} type="back-button" theme="primary" w="60px" posX="20px" posY="20px"/>
         </div>}
-        {props.BookId!=undefined && <Forms isOpen={showForm} onClose={()=>setShowForm(false)} id="create-chapter" onSubmit={(e)=>onSubmit(e, `http://localhost:5000/api/books/${props.BookId.toString()}/chapters`)}>
+        <Forms isOpen={showForm} onClose={()=>setShowForm(false)} id="create-chapter" onSubmit={(e)=>onSubmit(e, `http://localhost:5000/api/books/${props.BookId.toString()}/chapters`)}>
             <Forms.Input type="text" name="name" placeholder="Name:" required/>
-        </Forms>}
+        </Forms>
     </>
 }
 export default Modals;
